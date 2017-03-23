9f46fc25bb3a0b1026f8e7c59dc3e6500ae20979
thomasbrunier <thomasbrunier@gmail.com>
2017-03-23 17:41:59 +0100

    Action de la page profil d'un restaurant + view (WIP)

--- src/FaimfonyBundle/Controller/RestauController.php
+++ src/FaimfonyBundle/Controller/RestauController.php
@@ -8,6 +8,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 
 class RestauController extends Controller{
+
     /**
      * @Route("restaurant/create/", name="add_restau")
      */
@@ -21,4 +22,49 @@ public function restauAction(){
     return $this->render('FaimfonyBundle:Default:restaurantFormulaire.html.twig', array('form'=>$form->createView()
     ));
 }
+
+    /**
+     * @Route("restaurant/{id}", name="profil_restau")
+     */
+
+    public function profilRestauAction($id){
+
+//        $restau = $this->getRestau($id);
+
+//        $urlPhoto   = $restau->getUrlPhoto;
+//        $name       = $restau->getName;
+//        $address    = $restau->getAdress;
+//        $timetable  = $restau->getTimetable;
+//        $phone      = $restau->getPhone;
+//        $mealsList  = $restau->getMealsList;
+
+        $urlPhoto   = "http://placehold.it/1280x840";
+        $name       = 'La buvette';
+        $address    = "100, rue de l'adresse";
+        $timetable  = "{}";
+        $phone      = '1234567890';
+        $mealsList  = [];
+
+
+        $user = $this->getUser();
+        if($user != null){
+//            If restau belongs to user
+            $userIsOwner = true;
+        }
+        else{
+            $userIsOwner = false;
+        }
+
+        return $this->render('FaimfonyBundle:Default:restauProfil.html.twig', array(
+            'id'=>$id,
+            'userIsOwner'=>$userIsOwner,
+            'urlPhoto'=>$urlPhoto,
+            'name'=>$name,
+            'address'=>$address,
+            'timetable'=>$timetable,
+            'phone'=>$phone,
+            'mealsList'=>$mealsList
+        ));
+    }
+
 }
