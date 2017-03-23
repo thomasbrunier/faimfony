33dd9f3981a752ae2593a4750688772affe3d440
bonaccorsoN <nicola.bonaccorso@gmail.com>
2017-03-23 16:56:11 +0100

    ajout des restaurant dans la base conversion des horaires en tableau json

--- src/FaimfonyBundle/Controller/RestauController.php
+++ src/FaimfonyBundle/Controller/RestauController.php
@@ -5,6 +5,7 @@ use FaimfonyBundle\Entity\Restaurant;
 use FaimfonyBundle\Form\RestaurantType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 
 class RestauController extends Controller{
@@ -12,12 +13,32 @@ class RestauController extends Controller{
      * @Route("restaurant/create/", name="add_restau")
      */
 
-public function restauAction(){
+public function restauAction(Request $request){
 
-    $Restaurant = new Restaurant();
+    $restaurant = new Restaurant();
 
-    $form = $this->CreateForm(RestaurantType::class, $Restaurant);
+    $form = $this->CreateForm(RestaurantType::class, $restaurant);
+    $user = $this->getUser();
+    $form->handleRequest($request);
+    $restaurant->setUser($user);
+    $dayArray = ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi','dimanche'];
+    $timetableArray = [];
+    foreach ($dayArray as $day => $time){
+        $timetableArray[$time][] = $form->get($time.':ouverture')->getData();
+        $timetableArray[$time][] = $form->get($time.':fermeture')->getData();
+        //array_push($timetableArray, $form->get($day.':ouverture')->getData());
+        //array_push($timetableArray ,$form->get($day.':fermeture')->getData());
 
+    }
+    if($form->isSubmitted() && $form->isValid()){
+        $timetableArray = json_encode($timetableArray);
+        $restaurant->setTimetable($timetableArray);
+        dump($timetableArray);
+        die();
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($restaurant);
+        $em->flush();
+    }
     return $this->render('FaimfonyBundle:Default:restaurantFormulaire.html.twig', array('form'=>$form->createView()
     ));
 }
