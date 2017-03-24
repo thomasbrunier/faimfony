aeb23a032b1c5bc6236e8c5d57ff72f6d563e240
thomasbrunier <thomasbrunier@gmail.com>
2017-03-24 14:59:23 +0100

    Mise en place controller restau/meals : retrieve (WIP)

--- src/FaimfonyBundle/Controller/RestauController.php
+++ src/FaimfonyBundle/Controller/RestauController.php
@@ -1,6 +1,7 @@
 <?php
 namespace FaimfonyBundle\Controller;
 
+use FaimfonyBundle\Entity\Meal;
 use FaimfonyBundle\Entity\Restaurant;
 use FaimfonyBundle\Form\RestaurantType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -8,79 +9,104 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 
 
-class RestauController extends Controller{
+class RestauController extends Controller
+{
 
     /**
      * @Route("restaurant/create/", name="add_restau")
      */
 
-public function restauAction(Request $request){
+    public function restauAction(Request $request)
+    {
 
-    $restaurant = new Restaurant();
+        $restaurant = new Restaurant();
 
-    $form = $this->CreateForm(RestaurantType::class, $restaurant);
-    $user = $this->getUser();
-    $form->handleRequest($request);
-    $restaurant->setUser($user);
-    $dayArray = ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi','dimanche'];
-    $timetableArray = [];
-    foreach ($dayArray as $day => $time){
-        $timetableArray[$time][] = $form->get($time.':ouverture')->getData();
-        $timetableArray[$time][] = $form->get($time.':fermeture')->getData();
-        //array_push($timetableArray, $form->get($day.':ouverture')->getData());
-        //array_push($timetableArray ,$form->get($day.':fermeture')->getData());
+        $form = $this->CreateForm(RestaurantType::class, $restaurant);
+        $user = $this->getUser();
+        $form->handleRequest($request);
+        $restaurant->setUser($user);
+        $dayArray = ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche'];
+        $timetableArray = [];
+        foreach ($dayArray as $day => $time) {
+            $timetableArray[$time][] = $form->get($time . ':ouverture')->getData();
+            $timetableArray[$time][] = $form->get($time . ':fermeture')->getData();
+            //array_push($timetableArray, $form->get($day.':ouverture')->getData());
+            //array_push($timetableArray ,$form->get($day.':fermeture')->getData());
 
+        }
+        if ($form->isSubmitted() && $form->isValid()) {
+            $timetableArray = json_encode($timetableArray);
+            $restaurant->setTimetable($timetableArray);
+            dump($timetableArray);
+            die();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($restaurant);
+            $em->flush();
+        }
+        return $this->render('FaimfonyBundle:Default:restaurantFormulaire.html.twig', array('form' => $form->createView()
+        ));
     }
-    if($form->isSubmitted() && $form->isValid()){
-        $timetableArray = json_encode($timetableArray);
-        $restaurant->setTimetable($timetableArray);
-        dump($timetableArray);
-        die();
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($restaurant);
-        $em->flush();
-    }
-    return $this->render('FaimfonyBundle:Default:restaurantFormulaire.html.twig', array('form'=>$form->createView()
-    ));
-}
 
     /**
      * @Route("restaurant/{id}", name="profil_restau")
      */
 
-    public function profilRestauAction($id){
+    public function profilRestauAction($id)
+    {
+
+//        $restauRepository = $this->getDoctrine()->getRepository(Restaurant::class);
+//        $restaus = $restauRepository->findAll();
 
         $restauRepository = $this->getDoctrine()->getRepository(Restaurant::class);
-        $restaus = $restauRepository->findAll();
+        $restau = $restauRepository->find($id);
+
+        $MealRepository = $this->getDoctrine()->getRepository(Meal::class);
+//        $meals = $MealRepository->findByRestau($id);
+
+        $meals = [
+            'meal1' => [
+                'description' => '',
+                'id' => '1',
+                'image_id' => '',
+                'name' => 'burger',
+                'price' => ''
+            ],
+            'meal2' => [
+                'description' => '',
+                'id' => '2',
+                'image_id' => '',
+                'name' => 'salade',
+                'price' => ''
+            ]
+        ];
 
 //        $restau = new Restaurant();
 
-        $timetable  = "{}";
-        $mealsList  = [];
+//        $timetable  = "{}";
+//        $mealsList  = [];
 
-        $restau = [
-            'urlPhoto'=>"http://placehold.it/1280x840",
-            'name'=>'La buvette',
-            'address'=>"100, rue de l'adresse",
-            'timetable'=>$timetable,
-            'phone'=>'1234567890',
-            'mealsList'=>$mealsList
-        ];
+//        $restau = [
+//            'urlPhoto'=>"http://placehold.it/1280x840",
+//            'name'=>'La buvette',
+//            'address'=>"100, rue de l'adresse",
+//            'timetable'=>$timetable,
+//            'phone'=>'1234567890',
+//            'mealsList'=>$mealsList
+//        ];
 
         $user = $this->getUser();
-        if($user != null){
+        if ($user != null) {
 //            If restau belongs to user
             $userIsOwner = true;
-        }
-        else{
+        } else {
             $userIsOwner = false;
         }
 
         return $this->render('FaimfonyBundle:Default:restauProfil.html.twig', array(
-            'id'=>$id,
-            'userIsOwner'=>$userIsOwner,
-            'restau'=>$restau,
-            'restaus'=>$restaus
+            'id' => $id,
+            'userIsOwner' => $userIsOwner,
+            'restau' => $restau,
+            'meals' => $meals
         ));
     }
 
