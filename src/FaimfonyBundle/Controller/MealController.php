71f4cc700b6003bd37d5caeb059a6c8c00565d6f
alexandrepol <alexandre.pol1996@gmail.com>
2017-03-27 11:06:03 +0200

    - Meal edit / delete

--- src/FaimfonyBundle/Controller/MealController.php
+++ src/FaimfonyBundle/Controller/MealController.php
@@ -7,6 +7,7 @@ use FaimfonyBundle\Form\MealType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 
 class MealController extends Controller
@@ -25,7 +26,6 @@ class MealController extends Controller
         $meal = new Meal();
 
         $form = $this->CreateForm(MealType::class, $meal);
-//        $user = $this->getUser();
         $form->handleRequest($request);
         $meal->setRestaurant($restau);
 
@@ -39,5 +39,41 @@ class MealController extends Controller
         ));
     }
 
+    /**
+     * @Route("meal/edit/{id}", name="edit_meal")
+     */
+    public function mealEditAction(Request $request, $id){
+        $meal = $this->getDoctrine()->getRepository(Meal::class)->find($id);
+        $form = $this->createForm(MealType::class, $meal);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($meal);
+            $em->flush();
+            return $this->redirect($this->generateUrl('edit_meal', array('id'=>$meal->getId())));
+        }
+        return $this->render('FaimfonyBundle:Default:editMeal.html.twig', array('meal'=>$meal ,'form' => $form->createView()
+        ));
+    }
+
+    /**
+     * @Route("meal/delete/{id}", name="delete_meal")
+     */
+    public function mealDeleteAction(Request $request, $id){
+        $session = new Session();
+        $user = $this->getUser();
+        $meal = $this->getDoctrine()->getRepository(Meal::class)->find($id);
+        if($user == $meal->getRestaurant()->getUser()){
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($meal);
+            $em->flush();
+            $session->getFlashBag()->add('notice', 'Votre plat a été supprimé');
+            return $this->redirect($this->generateUrl('user_profil'));
+        }
+        else{
+            return $this->redirect($this->generateUrl('user_profil'));
+        }
+    }
 
 }
